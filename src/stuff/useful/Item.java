e9894f9ab99d033d8e3345701ffa180f0a38f45b
Yannick Plante <yannick.plante16@hotmail.com>
2020-02-24 21:39:56 -0500

    Added Item agaain

---
+++ src/stuff/useful/Item.java
@@ -0,0 +1,18 @@
+package stuff.useful;
+
+import javafx.scene.image.Image;
+
+public enum Item {
+	
+	BURGER("burger", "../resources/burger.png");
+	
+	private String name;
+	private Image img;
+	
+	private Item(String name, String img) {
+		this.name = name;
+		
+		this.img = new Image(img);
+	}
+
+}
